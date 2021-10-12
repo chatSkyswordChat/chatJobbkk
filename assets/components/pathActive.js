ec05c2bc530c6f40305f40b8e67dbae31f619bb3
chatthong <chatthong_hon@jobbkk.co.th>
2021-10-12 13:25:00 +0700

    uuuu

--- assets/components/pathActive.js
+++ assets/components/pathActive.js
@@ -1,5 +1,5 @@
 // const path = location.href.split("/").length === 6 ? "" : "../";
-const path = location.href.split("/").length === 3 ? "" : "../";
+const path = location.href.split("/");
 console.log(path);
 
 // active menu
